f4d60b4f8019b4e18401e112be84fc15ff489d50
Gonzalo Ebel <gonzalonicolasebel@gmail.com>
2024-04-02 19:47:42 -0300

    Lab3

---
+++ StringUtilidades/Program.cs
@@ -0,0 +1,20 @@
+﻿string nombre = "Juan";
+string apellido = "Perez";
+
+Console.WriteLine("Nombre: " + nombre + " Apellido: " + apellido);
+
+int longitudNombre = nombre.Length;
+Console.WriteLine("Longitud del nombre: " + longitudNombre);
+
+string frase = "La facultad UTN es la mejor";
+string palabra = "UTN";
+
+// Contains devuelve un booleano si la palabra esta en la frase
+bool contiene = frase.Contains(palabra);
+Console.WriteLine(contiene);
+
+// IndexOf devuelve el indice de la palabra en la frase
+int indice = frase.IndexOf(palabra);
+Console.WriteLine("Indice de la palabra: " + indice);
+
+string fraseCambiada = frase.Replace("facultad", "universidad");
