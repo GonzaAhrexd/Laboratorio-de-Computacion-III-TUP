828276669266b34d97daecbaeb0d23982d02e224
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-04-16 19:27:07 -0300

    Reestructuración

---
+++ 1- ConceptosBásicos/ObtenerFactorial/Program.cs
@@ -0,0 +1,17 @@
+﻿int obtenerFactorial(int num)
+{
+    if (num == 1)
+    {
+        return num;
+    }
+    else
+    {
+        return num * obtenerFactorial(num - 1);
+    }
+}
+
+
+int aFactorear = int.Parse(Console.ReadLine());
+int numeroFactoreado = obtenerFactorial(aFactorear);
+Console.WriteLine(numeroFactoreado);
+
---
+++ 4- Programación Orientada a Objetos/Ej1-Zoologico/Cuidador.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej1_Zoologico
+{
+    public class Cuidador{
+        private String nombre { get; set; }
+        private String  { get; set; }
+}
+}
